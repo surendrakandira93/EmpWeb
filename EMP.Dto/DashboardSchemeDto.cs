2eeaac5fb93be8e2395788162a58c1626eaa7a0a
Surendra Kandira <surendrakandira@gmail.com>
2022-02-09 13:18:03 +0530

    Working on dashboard

---
+++ EMP.Dto/DashboardSchemeDto.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMP.Dto
+{
+    public class DashboardSchemeDto
+    {
+        public DashboardSchemeDto()
+        {
+            this.Result = new List<DashboardItemsSchemeDto>();
+        }
+        public string Name { get; set; }
+        public string Header { get; set; }
+        public List<DashboardItemsSchemeDto> Result { get; set; }
+
+    }
+
+    public class DashboardItemsSchemeDto
+    {
+        public DashboardItemsSchemeDto()
+        {
+            this.History = new List<DashboardSchemeHistoryDto>();
+        }
+        public string Instrumen { get; set; }
+        public double LPT { get; set; }
+        public double LT { get; set; }
+        public double Chg { get; set; }
+        public List<DashboardSchemeHistoryDto> History { get; set; }
+    }
+
+    public class DashboardSchemeHistoryDto
+    {
+        public string Date { get; set; }
+        public double Price { get; set; }
+        public double Open { get; set; }
+        public double High { get; set; }
+        public double Low { get; set; }
+        public double Chg { get; set; }
+    }
+
+
+    public class DashboardItemsScheme2Dto
+    {
+        public DateTime EntryTime { get; set; }
+        public DateTime ExitTime { get; set; }
+        public string Instrument { get; set; }
+        public int Qty { get; set; }
+        public double EntryPrice { get; set; }
+        public double ExitPrice { get; set; }
+        public double LTP { get; set; }
+        public double PL { get; set; }
+        public double Chg { get; set; }
+        public double SL { get; set; }
+        public double Trgt { get; set; }
+    }
+
+    public class DashboardScheme2Dto
+    {
+        public DashboardScheme2Dto()
+        {
+            this.Result = new List<DashboardItemsScheme2Dto>();
+        }
+        public string Name { get; set; }
+        public string Header { get; set; }
+        public List<DashboardItemsScheme2Dto> Result { get; set; }
+    }
+}
--- EMP.Web/Controllers/HomeController.cs
+++ EMP.Web/Controllers/HomeController.cs
@@ -1,9 +1,14 @@
-﻿using EMP.Web.Models;
+﻿using EMP.Dto;
+using EMP.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,15 +17,19 @@ namespace EMP.Web.Controllers
     public class HomeController : BaseController
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IHostingEnvironment env;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IHostingEnvironment _env)
         {
             _logger = logger;
+            this.env = _env;
         }
 
+        [AllowAnonymous]
         public IActionResult Index()
         {
-            return View();
+            var response = GetData();
+            return View(response);
         }
 
         public IActionResult Profile()
@@ -32,6 +41,17 @@ namespace EMP.Web.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public List<DashboardScheme2Dto> GetData()
+        {
+            var webRoot = env.WebRootPath;
+            string filePath = $"{webRoot}/js/Dashboard-Scheme2.json";
+            List<DashboardScheme2Dto> dataResponse = new List<DashboardScheme2Dto>();
+            using (var sr = new StreamReader(filePath))
+            {
+                return JsonConvert.DeserializeObject<List<DashboardScheme2Dto>>(sr.ReadToEnd()).ToList();
+            }
+
         }
     }
 }
